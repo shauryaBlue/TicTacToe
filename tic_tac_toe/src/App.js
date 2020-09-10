562c21dac3ccb138c6bc67e6dee6da018ca041ff
Shaurya <shauryamishra2279@gmail.com>
2020-09-10 16:41:37 +0530

    Winning condition added

--- tic_tac_toe/src/App.js
+++ tic_tac_toe/src/App.js
@@ -27,6 +27,13 @@ class Board extends React.Component {
 		});
 	}
 
+	gameOver() {
+		this.setState({
+			squares: Array(9).fill(null),
+			xIsNext: true
+		});
+	}
+
 	renderSquare(i) {
 		return (
 			<Square
@@ -37,7 +44,14 @@ class Board extends React.Component {
 	}
 
 	render() {
-		const status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+		const winner = calculateWinner(this.state.squares);
+		let status;
+		if (winner) {
+			status = "Winner: " + winner;
+			this.gameOver();
+		} else {
+			status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+		}
 
 		return (
 			<div>
@@ -62,6 +76,29 @@ class Board extends React.Component {
 	}
 }
 
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6]
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (
+			squares[a] &&
+			squares[a] === squares[b] &&
+			squares[a] === squares[c]
+		) {
+			return squares[a];
+		}
+	}
+	return null;
+}
 class App extends React.Component {
 	render() {
 		return (
